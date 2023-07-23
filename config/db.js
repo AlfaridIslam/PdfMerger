716ed924af3373c5f172575582bcd534f0a5b935
alfarid <alfaridislam@gmail.com>
2023-07-23 18:52:34 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit the process with failure
+    // Exit process with failure
     process.exit(1);
   }
 };
